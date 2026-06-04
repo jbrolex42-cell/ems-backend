4eaf356a2bf6eb6d7f1cde59f87f0c4f43b064fe
jbrolex42-cell <jbrolex42@gmail.com>
2026-06-04 14:09:56 +0300

    fixed backend

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,19 +1,34 @@
 const jwt = require('jsonwebtoken');
 
 const generateToken = (id, role) => {
-  return jwt.sign({ id, role }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRE,
-  });
+  return jwt.sign(
+    { id, role },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: process.env.JWT_EXPIRE || '7d'
+    }
+  );
 };
 
 const generateRefreshToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_REFRESH_SECRET, {
-    expiresIn: process.env.JWT_REFRESH_EXPIRE,
-  });
+  return jwt.sign(
+    { id },
+    process.env.JWT_REFRESH_SECRET,
+    {
+      expiresIn: process.env.JWT_REFRESH_EXPIRE || '30d'
+    }
+  );
 };
 
 const verifyToken = (token) => {
-  return jwt.verify(token, process.env.JWT_SECRET);
+  return jwt.verify(
+    token,
+    process.env.JWT_SECRET
+  );
 };
 
-module.exports = { generateToken, generateRefreshToken, verifyToken };
+module.exports = {
+  generateToken,
+  generateRefreshToken,
+  verifyToken
+};
